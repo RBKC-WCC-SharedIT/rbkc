3b77e833a084644c75c9b161b3afbe47e677b913
James Panton <james.panton@codeenigma.com>
2015-04-29 16:21:59 +0100

    r8044 Added rbkc_fe module, currently implementing hook_form_node_form_alter to add javascript to theme scald atoms.

---
+++ www/sites/all/modules/custom/rbkc_fe/js/scald.js
@@ -0,0 +1,29 @@
+(function($, Drupal) {
+  /**
+   * Override default atom theme function.
+   */
+  Drupal.theme.scaldEmbed = function(atom, context, options) {
+    context = context ? context : Drupal.settings.dnd.contextDefault;
+    var classname = 'image-cap dnd-atom-wrapper';
+    classname += ' type-' + atom.meta.type;
+    classname += ' context-' + context;
+    if (atom.meta.align && atom.meta.align != 'none') {
+      classname += ' atom-align-' + atom.meta.align;
+      classname += ' float' + atom.meta.align;
+    }
+
+    var output = '<div class="' + classname + '"><div class="dnd-drop-wrapper">' + atom.contexts[context] + '</div>';
+    if (atom.meta.legend) {
+      output += '<div class="image-cap__text dnd-legend-wrapper">' + atom.meta.legend + '</div>';
+    }
+    output += '</div>';
+
+    // If there are options, update the SAS representation.
+    if (options) {
+      output = output.replace(/<!-- scald=\d+(.+?) -->/, '<!-- scald=' + atom.sid + ':' + context + ' ' + JSON.stringify(options) + ' -->');
+    }
+
+    return output;
+  };
+
+}) (jQuery, Drupal);  
