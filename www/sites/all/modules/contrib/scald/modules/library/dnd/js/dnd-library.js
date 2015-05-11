30bf11f92d3f652ac0064e819752950946ed5af5
Mark Davies <mark.davies@codeenigma.com>
2015-05-11 16:01:42 +0100

    Updating sclad to latest dev release to work with scald_maps

--- www/sites/all/modules/contrib/scald/modules/library/dnd/js/dnd-library.js
+++ www/sites/all/modules/contrib/scald/modules/library/dnd/js/dnd-library.js
@@ -18,8 +18,8 @@ Drupal.dnd = {
   // Keep track of the last focused textarea.
   lastFocus: null,
 
-  // Setting for the qTip v2 library
-  qTipSettings: {
+  // Default settings for the qTip v2 library
+  defaultqTipSettings: {
     position: {
       my: 'right center',
       at: 'left center'
@@ -162,10 +162,10 @@ Drupal.dnd = {
    * Insert an atom in the current RTE or textarea.
    */
   insertAtom: function(sid) {
-    var cke = Drupal.ckeditorInstance;
-    if (cke) {
-      var markup = Drupal.theme('scaldEmbed', Drupal.dnd.Atoms[sid]);
-      cke.insertElement(CKEDITOR.dom.element.createFromHtml(markup));
+    var editor = Drupal.ckeditorInstance;
+    if (editor && editor.dndInsertAtom) {
+      // Defer to the correct method given the plugin used by this editor.
+      editor.dndInsertAtom(sid);
     }
     else if (Drupal.dnd.lastFocus) {
       var markup = Drupal.dnd.Atoms[sid].sas;
@@ -292,7 +292,15 @@ renderLibrary: function(data, editor) {
 
     // And add a nice preview behavior if qTip is present
     if ($.prototype.qtip) {
-      var settings = $.extend(Drupal.dnd.qTipSettings, {
+      if (Drupal.settings.dnd.qTipSettings === '') {
+        Drupal.settings.dnd.qTipSettings = Drupal.dnd.defaultqTipSettings;
+      }
+      else {
+        if (typeof Drupal.settings.dnd.qTipSettings !== 'object') {
+          Drupal.settings.dnd.qTipSettings = JSON.parse(Drupal.settings.dnd.qTipSettings);
+        }
+      }
+      var settings = $.extend(Drupal.settings.dnd.qTipSettings, {
         content: {
           text: Drupal.dnd.Atoms[atom_id].preview
         }
@@ -365,6 +373,7 @@ renderLibrary: function(data, editor) {
         return true;
       })
       .bind('dragend', function(e) {
+        delete Drupal.dnd.currentAtom;
         return true;
       });
   });
