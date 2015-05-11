269fc0ccac045d2742497913b5e4855d6624f918
Mark Davies <mark.davies@codeenigma.com>
2015-05-11 11:34:42 +0100

    Scald_maps and its dependencies

---
+++ www/sites/all/modules/contrib/scald_map/scald-map-render.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for Scald Map Render.
+ */
+?>
+<iframe width="600" height="450" frameborder="0" style="border:0"
+        src="/scald/map/render/<?php print $vars['id'] . '/' . $vars['zoomlevel']; ?>"></iframe>
