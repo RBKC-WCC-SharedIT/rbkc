b36346633db714d2eee66875e0f3dc14ebee6276
matason <chrispmaiden@gmail.com>
2015-05-14 10:48:29 +0100

    Added checks to prevent notices in the answer page template.

--- www/sites/all/themes/rbkc/templates/node--answer.tpl.php
+++ www/sites/all/themes/rbkc/templates/node--answer.tpl.php
@@ -14,17 +14,17 @@
                   </div><!-- end col8 -->
 
                   <div class="col4"  role="complementary">
-                    <?php if ($content['rbkc_related_content']['more_in']['#markup'] || $content['rbkc_related_content']['also_in']['#markup']): ?>
+                    <?php if (isset($content['rbkc_related_content']['more_in']['#markup']) || isset($content['rbkc_related_content']['also_in']['#markup'])): ?>
                     <div class="bordered related">
                       <!-- child menu -->
-                      <?php if ($content['rbkc_related_content']['more_in']['#markup']): ?>
+                      <?php if (isset($content['rbkc_related_content']['more_in']['#markup'])): ?>
                       <div class="related__more">
                         <h2 class="related__header">More in <?php print $title ?></h2>
                         <?php print render($content['rbkc_related_content']['more_in']); ?>
                       </div><!-- end related-more -->
                       <?php endif; ?>
 
-                      <?php if ($content['rbkc_related_content']['also_in']['#markup']): ?>
+                      <?php if (isset($content['rbkc_related_content']['also_in']['#markup'])): ?>
                       <!-- sibling menu -->
                       <div class="related__elsewhere">
                         <?php print render($content['rbkc_related_content']['also_in']); ?>
