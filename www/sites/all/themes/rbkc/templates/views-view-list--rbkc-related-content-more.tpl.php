0911971b3750d836e15583ebbe7a7861ac365a5c
matason <chrispmaiden@gmail.com>
2015-05-14 14:49:00 +0100

    Fix to the more in related content link on the answer page.
    Switched to using service hub term instead of topic as this seems to
    more accurately fit with the requirement.

--- www/sites/all/themes/rbkc/templates/views-view-list--rbkc-related-content-more.tpl.php
+++ www/sites/all/themes/rbkc/templates/views-view-list--rbkc-related-content-more.tpl.php
@@ -1,7 +1,3 @@
-<?php print $wrapper_prefix; ?>
-<?php if (!empty($title)) : ?>
-  <h3><?php print $title; ?></h3>
-<?php endif; ?>
 <?php print $list_type_prefix; ?>
 <?php $count = 0; ?>
 <?php foreach ($rows as $id => $row): ?>
@@ -9,4 +5,3 @@
   <li class="<?php print $classes_array[$id]; ?><?php print (($count >= 4) ? ' hide' : ''); ?>"><?php print $row; ?></li>
 <?php endforeach; ?>
 <?php print $list_type_suffix; ?>
-<?php print $wrapper_suffix; ?>
