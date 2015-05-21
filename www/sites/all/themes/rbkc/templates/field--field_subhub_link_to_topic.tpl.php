cf18a33fc7968d8a1b26e3d12b9b0524c0fe6d88
Sophie Payne <Sophie.Payne@rbkc.gov.uk>
2015-05-21 11:26:02 +0100

    NEEDS REVIEW - found title variable for H2 tags on Sub Hub page, does not look safe

--- www/sites/all/themes/rbkc/templates/field--field_subhub_link_to_topic.tpl.php
+++ www/sites/all/themes/rbkc/templates/field--field_subhub_link_to_topic.tpl.php
@@ -2,7 +2,10 @@
 <div class="row sub-hub-topic blue-border-top">
   <section>
   <div class="col5">
-    <h2><?php  print render($item['#node']->title);?></h2>
+    <h2><?php  //print render($item['#node']->title);
+              print render( $item['node'][21]['field_topic_link']['#object']->title);
+        ?>
+    </h2>
   </div>
   <div class="col7">
     <?php  print render($item);?>
