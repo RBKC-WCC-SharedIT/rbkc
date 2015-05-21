99cb9d865dc0db701df376cf01cad91af3d17fbb
Sophie Payne <Sophie.Payne@rbkc.gov.uk>
2015-05-21 12:12:30 +0100

    Another fix for subhub template

--- www/sites/all/themes/rbkc/templates/field--field_subhub_link_to_topic.tpl.php
+++ www/sites/all/themes/rbkc/templates/field--field_subhub_link_to_topic.tpl.php
@@ -1,17 +1,7 @@
 <?php foreach ($items as $delta => $item): ?>
 <div class="row sub-hub-topic blue-border-top">
   <section>
-  <div class="col5">
-    <h2>
-      <?php
-        //print render($item['#node']->title);
-        print render($item['node'][21]['field_topic_link']['#object']->title);
-      ?>
-    </h2>
-  </div>
-  <div class="col7">
-    <?php  print render($item);?>
-  </div><!--end col 7-->
+      <?php  print render($item);?>
   </section>
 </div> <!--end blueBorderTop-->
 <?php endforeach; ?>
--- www/sites/all/themes/rbkc/templates/node--topic--top.tpl.php
+++ www/sites/all/themes/rbkc/templates/node--topic--top.tpl.php
@@ -1,6 +1,13 @@
-<?php
-  for ( $i = 0; $i < 3; $i++ ) {
-    print render($content['field_topic_link'][$i]);
-  }
-?>
-<h3><a href="<?php print $node_url;?>">More in <?php print $content['field_topic_link']['#object']->title;?>...</a></h3>
+<div class="col5">
+  <h2>
+    <?php print render($content['field_topic_link']['#object']->title);?>
+  </h2>
+</div>
+<div class="col7">
+  <?php
+    for ( $i = 0; $i < 3; $i++ ) {
+      print render($content['field_topic_link'][$i]);
+    }
+  ?>
+  <h3><a href="<?php print $node_url;?>">More in <?php print render($content['field_topic_link']['#object']->title);?>...</a></h3>
+</div><!--end col 7-->
