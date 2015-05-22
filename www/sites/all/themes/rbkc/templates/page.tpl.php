5e2b3d6b39badbde9e5343386202d05fe46928b4
Sophie Payne <Sophie.Payne@rbkc.gov.uk>
2015-05-22 16:03:57 +0100

    sitestat

--- www/sites/all/themes/rbkc/templates/page.tpl.php
+++ www/sites/all/themes/rbkc/templates/page.tpl.php
@@ -94,3 +94,49 @@
   <?php print render($page['govmetric']); ?>
 </div><!-- end outerwrap -->
 
+<div id="sitestat"
+data-page="<?php $title = strtolower($title); $statTitle = preg_replace("/[\s_]/", "-", $title); print $statTitle; ?>"
+data-hub="<?php if (isset($service_area_link)): ?>
+            <?php $service_area_link = strtolower($service_area_link); $statHub = preg_replace("/[\s_]/", "-", $service_area_link); print $statHub; ?>
+          <?php endif; ?> ">
+
+<script type="text/javascript">
+ //<!-- Begin Sitestat4 Loadingtime1 code -->
+ns_loadingtime1=(new Date()).getTime();
+//<!-- End Sitestat4 Loadingtime1 code -->
+</script>
+<!-- Begin Sitestat4 code -->
+<script type="text/javascript">
+
+function getTitle() {
+var stats = document.getElementById('sitestat');
+return stats.dataset.page;
+};
+function getHub() {
+var stats = document.getElementById('sitestat');
+var hub = stats.dataset.hub;
+  if (hub !== " ") {
+    return hub + ".";
+  }
+};
+
+<!--
+function sitestat(ns_l){ns_l+='&amp;ns__t='+(new Date()).getTime();ns_pixelUrl=ns_l;
+ns_0=document.referrer;
+ns_0=(ns_0.lastIndexOf('/')==ns_0.length-1)?ns_0.substring(ns_0.lastIndexOf('/'),0):ns_0;
+if(ns_0.length>0)ns_l+='&amp;ns_referrer='+escape(ns_0);
+if(document.images){ns_1=new Image();ns_1.src=ns_l;}else
+document.write('<img src="'+ns_l+'" width="1" height="1" alt="" />');}
+sitestat("http://uk.sitestat.com/rbkc/rbkc/s?" + getTitle() + getHub());
+//-->
+</script>
+
+<div><img src="http://uk.sitestat.com/rbkc/rbkc/s?<?php if (isset($service_area_link)): ?><?php print $statHub."."; ?><?php endif; ?><?php print $statTitle; ?>" width="1" height="1" alt="" /></div>
+
+<!-- End Sitestat4 code -->
+<!-- Begin Sitestat4 Technical code --><script type="text/javascript" src="http://www.rbkc.gov.uk/sitestat.js"></script>
+<!-- End Sitestat4 Technical code -->
+<!-- Begin Sitestat4 Loadingtime2 code -->
+<script type="text/javascript">ns_loadingtime2=(new Date()).getTime();</script>
+<noscript><div>&nbsp;</div></noscript><!-- End Sitestat4 Loadingtime2 code -->
+</div>
