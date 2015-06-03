600b71eba0b086dad91de4dea0e0c1ec17725e41
Mark Davies <mark.davies@codeenigma.com>
2015-06-03 15:45:51 +0100

    Adding new layouts and panes used throughout the site

---
+++ www/sites/all/modules/custom/rbkc_sitewide/plugins/layouts/rbkc_newsroom/rbkc-newsroom.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * RBKC - Homepage Layout
+ */
+?>
+
+<div class="row">
+  <div class="col6"><?php print $content['row_one_col_one']; ?></div>
+  <div class="col6"><?php print $content['row_one_col_two']; ?></div>
+</div>
+<div class="row secondFlex">
+  <div class="col6"><?php print $content['row_two_col_one']; ?></div>
+  <div class="col6"><?php print $content['row_two_col_two']; ?></div>
+</div>
+
