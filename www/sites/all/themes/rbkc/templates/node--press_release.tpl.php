719035a8231a3029221bc5f7697d67cb53d006df
matason <chrispmaiden@gmail.com>
2015-06-19 06:09:29 +0100

    Show the created date on the press release page.

--- www/sites/all/themes/rbkc/templates/node--press_release.tpl.php
+++ www/sites/all/themes/rbkc/templates/node--press_release.tpl.php
@@ -1,6 +1,7 @@
                 <div class="row">
                   <div class="col12">
                     <div class="content">
+                      <p><?php print format_date($node->created, 'custom', 'd F Y'); ?></p>
                       <?php print render($content); ?>
                     </div>
                   </div><!-- end col8 -->
