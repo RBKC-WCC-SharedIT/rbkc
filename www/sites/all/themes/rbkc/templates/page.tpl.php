84e26b7f4541625b9f8308f9ea36510fff952e41
Sophie Payne <Sophie.Payne@rbkc.gov.uk>
2015-07-02 09:17:59 +0100

    hiding sitestat (I hope)

--- www/sites/all/themes/rbkc/templates/page.tpl.php
+++ www/sites/all/themes/rbkc/templates/page.tpl.php
@@ -95,7 +95,7 @@
   <?php print render($page['govmetric']); ?>
 </div><!-- end outerwrap -->
 
-<div id="sitestat" data-url="<?php
+<div class="element-invisible" id="sitestat" data-url="<?php
                               $statlink = str_replace('/', '.', request_uri());
                               print substr($statlink, 1);
                              ?>">
@@ -124,7 +124,7 @@ sitestat("http://uk.sitestat.com/rbkc/rbkc/s?" + getURL() + ".page" );
 //-->
 </script>
 
-<div><img src="https://uk.sitestat.com/rbkc/rbkc/s?<?php print substr($statlink, 1); ?>.page" width="1" height="1" alt="" /></div>
+<div ><img src="https://uk.sitestat.com/rbkc/rbkc/s?<?php print substr($statlink, 1); ?>.page" width="1" height="1" alt="" /></div>
 
 <!-- End Sitestat4 code -->
 <!-- Begin Sitestat4 Technical code --><script type="text/javascript" src="/js/rbkc/sitestat.js"></script>
